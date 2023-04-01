32f6a7efbb3fcdb4508ffe1dadf5406d43385f5b
izabela01 <63959868+izabela01@users.noreply.github.com>
2023-04-01 13:32:40 +0100

    accessing one property from object within array

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import {CarouselComp} from './features/CarouselComp.jsx'
 
 export function App() {
 
-    async function getData(){
-        let response = await fetch('https://api.themoviedb.org/3/movie/550?api_key=7ceb5ca6f93c5d8c993f1f5783f5ccf0');
+        // Console log the data 
+    async function getData(url){
+        // Fetch the data - useEffect or fetch 
+        let response = await fetch(url);
+
         let data = await response.json();
-        console.log(data);
-    }
+        console.log('**** Data Set ***** ', data);
+        console.log('*** accessing array ****', data.results);
 
-    getData();
+        // Cycling through the array, printing the title field of each movie object
+        data.results.forEach(movie => {
+                console.log('the movie title - ', movie.original_title);
+            });
+        }
+    // getData('https://api.themoviedb.org/3/movie/550?api_key=7ceb5ca6f93c5d8c993f1f5783f5ccf0');
+    getData('https://api.themoviedb.org/3/discover/movie?api_key=7ceb5ca6f93c5d8c993f1f5783f5ccf0&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate');
 
-// Fetch the data - useEffect or fetch 
 
-
-// Console log the data 
-// figure out the how to get it into individual components using props 
-// dont worry about components, do everything here for now 
+    // figure out the how to get it into individual components using props 
+    // dont worry about components, do everything here for now 
 
 
     return (
