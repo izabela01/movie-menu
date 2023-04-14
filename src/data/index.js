46f1a013eca0126b03c6a295b44b33f0cfadcaba
izabela01 <63959868+izabela01@users.noreply.github.com>
2023-04-14 16:52:51 +0100

    double actor search working

--- src/data/index.js
+++ src/data/index.js
@@ -11,6 +11,30 @@ export async function getMovies(url) {
   console.log('error: api request error');
 }
 
+async function getMoviesByActors(data) {
+  const actor1 = data[0].results[0].id;
+  const actor2 = data[1].results[0].id;
+  console.log('actor1', actor1);
+  console.log('actor2', actor2);
+
+  const urlStart = 'https://api.themoviedb.org/3/discover/movie?api_key=7ceb5ca6f93c5d8c993f1f5783f5ccf0&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&';
+  const urlEnd = '&with_watch_monetization_types=flatrate';
+  const middle = `with_people=${actor1},${actor2}`;
+
+  const response = await fetch(urlStart + middle + urlEnd);
+  const formattedResponse = await response.json();
+
+  console.log('formattedResponse', formattedResponse.results);
+
+  return formattedResponse.results;
+
+  // const movies = formattedResponse.filter((result) => result.known_for.length > 0);
+
+  // if (movies.length > 0) {
+  //   return movies[0].known_for;
+  // }
+}
+
 export async function getMoviesByActor(name, nameTwo) {
   console.log('names!! ', name, nameTwo);
   const actorsNames = [name, nameTwo];
@@ -29,7 +53,8 @@ export async function getMoviesByActor(name, nameTwo) {
 
   console.log('data in async', await data);
 
-  // const moviesResults = data.results;
+  const moviesList = getMoviesByActors(await data);
+  return moviesList;
   // const movies = moviesResults.filter((result) => result.known_for.length > 0);
 
   // if (movies.length > 0) {
